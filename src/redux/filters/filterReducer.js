4b56ded1c2cf53a454bfcc4d186ddeff39506b8e
hasanfuad <hasanmdfuad@hotmail.com>
2022-09-12 19:33:50 +0600

    commit filter reducer

---
+++ src/redux/filters/filterReducer.js
@@ -0,0 +1,36 @@
+import { COLORCHANGED, STATUSCHANGED } from "./actionTypes";
+import { initialState } from "./initialState";
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case STATUSCHANGED:
+      return {
+        ...state,
+        status: action.payload,
+      };
+
+    case COLORCHANGED:
+      const { color, changeType } = action.payload;
+
+      switch (changeType) {
+        case "added":
+          return {
+            ...state,
+            colors: [...state.color, color],
+          };
+        case "removed":
+          return {
+            ...state,
+            color: state.color.filter(
+              (existingColor) => existingColor !== color
+            ),
+          };
+        default:
+          return state;
+      }
+    default:
+      return state;
+  }
+};
+
+export default reducer;
